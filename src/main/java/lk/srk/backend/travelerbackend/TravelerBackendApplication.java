c4402a0cf65caa5fd301d713d5d1bf1cff485b51
SahanRansika <sahanransika07@gmail.com>
2026-07-29 16:53:50 +0530

    Backend Folders Upload completed🎉

---
+++ src/main/java/lk/srk/backend/travelerbackend/TravelerBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.srk.backend.travelerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/srk/backend/travelerbackend/TravelerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.srk.backend.travelerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
